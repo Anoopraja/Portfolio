add03fe85e69a7eb5ce639b290ea150e6a317193
Anoop Kumar <anooprajapati807@gmail.com>
2026-07-02 14:30:06 +0530

    footer name change

--- Portfolio/src/assets/components/Footer.jsx
+++ Portfolio/src/assets/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         
         {/* Profile Info */}
         <div>
-          <h3 className="text-3xl font-bold mb-2">Anoop Kumar</h3>
+          <h3 className="text-3xl font-bold mb-2">Anoop lofi</h3>
           <p className="text-gray-400 text-sm sm:text-base">
             React Developer &bull; PostgreSQL &bull; Sketch Artist &bull; Photography
           </p>
